e664e951bc5efbdb007acf9789c3229a20e3e3cb
iman <iman.parhizkari@gmail.com>
2018-08-20 21:03:18 +0430

    blog models and corresponding db models created

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+	first_name = models.CharField(max_length=30)
+	last_name = models.CharField(max_length=40)
+	email = models.EmailField()
+	created_date = models.DateTimeField()
+	
+	def __str__(self):
+		return self.first_name + " " + self.last_name
+
+class Comment(models.Model):
+	text = models.TextField()
+	author = models.ForeignKey(Author)
+	created_date = models.DateTimeField()
+	
+	def __str__(self):
+		return self.text
+
+class Post(models.Model):
+	title = models.CharField(max_length=100)
+	text = models.TextField("post text")
+	comments = models.ManyToManyField(Comment)
+	author = models.ForeignKey(Author)
+	created_date = models.DateTimeField()
+	
+	def __str__(self):
+		return self.title
