427dda375bc204b70ad17b7d3fc3aefcb0b3f602
iman <iman.parhizkari@gmail.com>
2018-08-21 19:45:46 +0430

    post url custom end url(delete $ from end of url re)

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.index_view, name='index'),
-	url(r'^post/(?P<post_id>[0-9]+)/$', views.post_view, name='post')
+	url(r'^post/(?P<post_id>[0-9]+)/', views.post_view, name='post')
 ]
