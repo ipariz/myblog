a3fc94bd523ed896f424d0b89f73c2da065268bc
iman <iman.parhizkari@gmail.com>
2018-09-02 21:22:05 +0430

    add comment to posts from comment form with no name author

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index_view, name='index'),
-    url(r'^post/(?P<post_id>[0-9]+)/', views.post_view, name='post')
+    url(r'^post/(?P<post_id>[0-9]+)/comment', views.comment_submit, name='comment'),
+    url(r'^post/(?P<post_id>[0-9]+)/', views.post_view, name='post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_protect
+from django.http import HttpResponseRedirect, HttpResponseNotFound
 from .models import Post, Author, Comment
+from datetime import datetime
+import pytz
 
 def index_view(request):
     posts = Post.objects.order_by('-created_date')[:10]
@@ -12,4 +16,28 @@ def post_view(request, post_id):
     comments = post.comments.order_by("-created_date")
     context = {'comments': comments, 'post': post, 'posts': posts}
     return render(request, "blog/post.html", context)
-    
+
+@csrf_protect
+def comment_submit(request, post_id):
+    if request.method == 'POST':
+        if request.POST['comment'] != '':
+            text = request.POST['comment']
+            no_author = Author.objects.get(pk=3)
+            aware_now = pytz.utc.localize(datetime.now())
+            comment = Comment.objects.create(text = text, author = no_author, created_date = aware_now)
+            post = Post.objects.get(pk=post_id)
+            post.comments.add(comment)
+            return HttpResponseRedirect("/blog/post/%s/" % post_id)
+        else:
+            #send error
+            return HttpResponseRedirect("/blog/post/%s/" % post_id)
+    return HttpResponseNotFound("Page not found")
+
+
+
+
+
+
+
+
+
